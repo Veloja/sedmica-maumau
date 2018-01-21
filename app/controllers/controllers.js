b8b8a8a434da9f5c7e47d4f6f8f952419eb15d28
Vladan <vladanvelojic@gmail.com>
2018-01-21 13:11:59 +0100

    no need for nested ng-repeat

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -4,45 +4,55 @@ app.controller('SedmickaController', function ($scope) {
 		{
 			id: 1,
 			totalScore: 0,
+			selected: false,
 			name: 'Veloja',
 			points: [],
+			hands: 0,
 		},
 		{
 			id: 2,
 			totalScore: 0,
+			selected: false,
 			name: 'Proda',
 			points: [],
+			hands: 0,
 		},
 		{
 			id: 3,
 			totalScore: 0,
+			selected: false,
 			name: 'Gile',
 			points: [],
+			hands: 0,
 		}
 	];
 
+	$scope.trackHands = 0;
 	$scope.trackRounds = 0;
+	$scope.players[$scope.trackRounds].selected = true;
+	$scope.trackPlayer = 0;
 	$scope.gameOver = false;
 	$scope.input = null;
 
-	$scope.score = function (input, player) {
+	$scope.submit = function (input, player) {
 		for (var i = 0; i < $scope.players.length; i++) {
 			if (player === $scope.players[i]) {
 				$scope.players[i].points.push(input);
-				console.log(player);
+				$scope.players[i].hands++;
 				totalScoreForPlayer(input, player);
-			}
-		}
-		$scope.input = '';
-	}
-// ovo za sada radi
-	function totalScoreForPlayer(input, player) {
-		var totalScore = input;
-		for(var i = 0; i < $scope.players.length; i++){
-			if(player === $scope.players[i]){
-				$scope.players[i].totalScore += totalScore;
-			}
+			} 
 		}
 	}
 
-});
+
+		// radi za nest ng-repeat
+		function totalScoreForPlayer(input, player) {
+			var totalScore = input;
+			for (var i = 0; i < $scope.players.length; i++) {
+				if (player === $scope.players[i]) {
+					$scope.players[i].totalScore += totalScore;
+				}
+			}
+		}
+
+	});
